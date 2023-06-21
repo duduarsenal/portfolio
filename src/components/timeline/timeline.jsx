c7dcde61de017a70e2945db43fa8956832be5485
Dudu <edudocross@gmail.com>
2023-06-21 01:25:47 -0300

    timeline responsive add

--- src/components/timeline/timeline.jsx
+++ src/components/timeline/timeline.jsx
@@ -3,58 +3,29 @@ import "./timeline.css";
 
 export default function Timeline() {
   return (
-    <div className="timeline w-[100%]">
-      <div className="timeline-content w-[100%] flex flex-col items-center">
-        <ul className="timelineTop flex justify-evenly items-center w-[90%]">
+    <div className="timeline w-[100%] flex justify-center">
+      <div className="timeline-content w-[97%] flex flex-col items-center border-solid border-red-transparent bg-black-transparent border-2 h-max my-[10px] rounded overflow-hidden">
+        <h1 className="mytimeline-title text-center font-semibold text-[200%] mt-[10px]">Minha Linha do Tempo</h1>
+        <ul className="timelineTop mb-[200px] flex justify-evenly items-center w-[90%]">
           <li className="itemTimeLine">
-            <div className="timeline-descricao"></div>
-            <div className="timeline-button"></div>
+            <h1 className="title-timeline">2019/1</h1>
+            <p className="desc-timeline">Meu primeiro contato na área de <span className="text-[rgb(255,45,45)]">programação</span> pela Faculdade de Analise e Desenvolvimento de Sistemas</p>
           </li>
           <li className="itemTimeLine">
-            <div className="timeline-descricao"></div>
-            <div className="timeline-button-not"></div>
+            <h1 className="title-timeline">2019/2</h1>
+            <p className="desc-timeline">Meu primeiro projeto feito na Linguagem C, um sistema de controle de uma pizzaria, feito em console application</p>
           </li>
           <li className="itemTimeLine">
-            <div className="timeline-descricao"></div>
-            <div className="timeline-button"></div>
+            <h1 className="title-timeline">2020/2021</h1>
+            <p className="desc-timeline">Tranquei o curso devido a pandemia</p>
           </li>
           <li className="itemTimeLine">
-            <div className="timeline-descricao"></div>
-            <div className="timeline-button-not"></div>
+            <h1 className="title-timeline">2022</h1>
+            <p className="desc-timeline">Voltei o curso ja iniciando um novo projeto, uma página de seguros residenciais, feito com Angular/Bootstrap/C#</p>
           </li>
           <li className="itemTimeLine">
-            <div className="timeline-descricao"></div>
-            <div className="timeline-button"></div>
-          </li>
-          <li className="itemTimeLine">
-            <div className="timeline-descricao"></div>
-            <div className="timeline-button-not"></div>
-          </li>
-        </ul>
-        <ul className="timelineBottom flex justify-evenly items-center flex-row-reverse w-[90%]">
-          <li className="itemTimeLine">
-            <div className="timeline-descricao"></div>
-            <div className="timeline-button"></div>
-          </li>
-          <li className="itemTimeLine">
-            <div className="timeline-descricao"></div>
-            <div className="timeline-button-not"></div>
-          </li>
-          <li className="itemTimeLine">
-            <div className="timeline-descricao"></div>
-            <div className="timeline-button"></div>
-          </li>
-          <li className="itemTimeLine">
-            <div className="timeline-descricao"></div>
-            <div className="timeline-button-not"></div>
-          </li>
-          <li className="itemTimeLine">
-            <div className="timeline-descricao"></div>
-            <div className="timeline-button"></div>
-          </li>
-          <li className="itemTimeLine">
-            <div className="timeline-descricao"></div>
-            <div className="timeline-button-not"></div>
+            <h1 className="title-timeline">2023</h1>
+            <p className="desc-timeline">Iniciei 3 novos projetos, além do meu portfólio e entrei mais afundo no mundo de dev, estudando ReactJS, PHP, MySQL e etc..</p>
           </li>
         </ul>
       </div>
