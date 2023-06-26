fa8058aaea326ab393dc8f75a6bae003238b65c9
Dudu <edudocross@gmail.com>
2023-06-25 23:04:14 -0300

    att style

--- src/components/myKnowledge/myKnowledge.jsx
+++ src/components/myKnowledge/myKnowledge.jsx
@@ -2,20 +2,20 @@ import React from "react";
 import Proficient from './tech/proficient'
 import Learning from './tech/learning'
 import WannaStudy from './tech/wannaStudy'
-import ContainerTechnologys from "./containerTech/containerTech";
+import ContainerTech from "./containerTech/containerTech";
 
 
 export default function MyKnowledge() {
   return (
     <div className="w-[100%] max-w-[1360px]" id="meusconhecimentos">
-      <div className="w-[100%] flex flex-col items-center justify-center">
-        <h1 className="w-[100%] text-center font-semibold text-[200%]">Tecnologias</h1>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-center font-semibold text-[200%]">Tecnologias</h1>
         <div className="w-[100%] flex flex-row sm:flex-col md:flex-row justify-evenly items-center h-max">
-          <ContainerTechnologys infos={<Proficient />} infoTitle="Proficiente"/>
-          <ContainerTechnologys infos={<Learning />} infoTitle="Aprendendo"/>
+          <ContainerTech infos={<Proficient />} infoTitle="Proficiente"/>
+          <ContainerTech infos={<Learning />} infoTitle="Aprendendo"/>
         </div>
-        <div className=" w-[100%] flex max-w-[1360px]">
-          <ContainerTechnologys infos={<WannaStudy />} infoTitle="Pretendo Estudar"/>
+        <div className="w-[100%] flex max-w-[1360px]">
+          <ContainerTech infos={<WannaStudy />} infoTitle="Pretendo Estudar"/>
         </div>
       </div>
     </div>
