524d7ed349b2eeb07b0689f0a28c38067b0ce49b
Eduardo "Dudu" Souza <111618029+duduarsenal@users.noreply.github.com>
2023-07-09 23:42:50 -0300

    Update contactMe.jsx

--- src/components/contactMe/contactMe.jsx
+++ src/components/contactMe/contactMe.jsx
@@ -7,7 +7,7 @@ export default function ContactMe() {
         <div className='contactme flex flex-col items-center justify-center my-[10px] mx-auto bg-black-transparent p-[20px] md:w-[70%] w-[90%] rounded-[5px] border-2 border-solid border-red-transparent min-w-[300px] max-w-[1360px]' id='contactme'>
             <h1 className='title-contato font-medium border-b-2 border-solid border-red-transparent leading-none mb-[10px] w-[100%] sm:w-[50%] text-center rounded-[20px] text-[180%]'>Entre em contato comigo!</h1>
             <div className="redes flex flex-col sm:flex-row w-[80%] justify-evenly items-center">
-                <a className="link-contato" href='https://github.com/Dudu-Arsenal' target='_blank'>
+                <a className="link-contato" href='https://github.com/duduarsenal' target='_blank'>
                     <BsGithub className='github icon-contato'/>
                     <p className='github'>GitHub</p>
                 </a>
